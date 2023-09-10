048ded33c03c50269e4fe2f5ec30be249cd824bc
velicanakcakaya <accounts@akcakaya.net>
2023-09-10 19:34:01 +0200

    Refactors getUsersBadge to use if statements

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import { User } from './types/user.interface';
-import { Icon } from './types/icon.enum';
+import {User} from './types/user.interface';
+import {Icon} from './types/icon.enum';
 
+/**
+ * Returns the correct badge for a user based on their `solution count`.
+ *
+ * @param user The user to get the badge for.
+ *
+ * @note This refactor removes the original `switch ( true )` statement and replaces it with a series of `if` statements.
+ *       Additionally, the initial null binding to a let variable is removed and a guard clause is prepended to the function.
+ */
 export const getUsersBadge = ( user: User ): Icon | null => {
-  let badge = null;
-  switch ( true ) {
-    case ( user.solutionCount >= 5 && user.solutionCount < 25 ):
-      badge = Icon.BADGE_BRONZE;
-      break;
-    case ( user.solutionCount >= 25 && user.solutionCount < 50 ):
-      badge = Icon.BADGE_SILVER;
-      break;
-    case ( user.solutionCount >= 50 ):
-      badge = Icon.BADGE_GOLD;
-      break;
+  if ( user.solutionCount < 5 ) {
+    return null;
   }
-  return badge;
-};
+
+  if ( user.solutionCount < 25 ) {
+    return Icon.BADGE_BRONZE;
+  }
+
+  if ( user.solutionCount < 50 ) {
+    return Icon.BADGE_SILVER;
+  }
+
+  return Icon.BADGE_GOLD;
+}
